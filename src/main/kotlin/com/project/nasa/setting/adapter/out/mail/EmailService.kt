db42159d73e85073bbca0d087e2a3aef55916a6f
Ewan Jee <830jiforcareer@gmail.com>
2024-04-05 16:57:43 -0400

    add/sendImage function

--- src/main/kotlin/com/project/nasa/setting/adapter/out/mail/EmailService.kt
+++ src/main/kotlin/com/project/nasa/setting/adapter/out/mail/EmailService.kt
@@ -1,4 +1,5 @@
 package com.project.nasa.setting.adapter.out.mail
 
 interface EmailService {
+    fun sendImage(to: String, subject: String, url: String)
 }
--- src/main/kotlin/com/project/nasa/setting/adapter/out/mail/EmailServiceImpl.kt
+++ src/main/kotlin/com/project/nasa/setting/adapter/out/mail/EmailServiceImpl.kt
@@ -1,18 +1,23 @@
 package com.project.nasa.setting.adapter.out.mail
 
+import org.springframework.core.io.InputStreamSource
+import org.springframework.core.io.UrlResource
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
 
 class EmailServiceImpl(
     private val mailSender: JavaMailSender
-) {
-    fun sendMail(to: String, subject: String, body: String) {
+) : EmailService {
+    override fun sendImage(to: String, subject: String, url: String) {
         val message = mailSender.createMimeMessage()
         try {
             val helper = MimeMessageHelper(message, true, "UTF-8")
             helper.setTo(to)
             helper.setSubject(subject)
-            helper.setText(body, true)
+            helper.setText(url, true)
+
+            val imageResource: InputStreamSource = UrlResource(url)
+            helper.addAttachment("APOD.jpg", imageResource)
             mailSender.send(message)
         } catch (e: Exception) {
             e.printStackTrace()
