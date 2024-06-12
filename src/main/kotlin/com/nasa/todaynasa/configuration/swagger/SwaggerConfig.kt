748a3dbd7eecc9eeac476829f9a1d3d4d148854f
Ewan Jee <830jiforcareer@gmail.com>
2024-06-12 11:45:22 +0900

    add / SwaggerConfig

---
+++ src/main/kotlin/com/nasa/todaynasa/configuration/swagger/SwaggerConfig.kt
@@ -0,0 +1,37 @@
+package com.nasa.todaynasa.configuration.swagger
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@OpenAPIDefinition(info = Info(title = "NASA API DOCS", description = "Description", version = "v1"))
+@Configuration
+class SwaggerConfig {
+    companion object {
+        private const val BEARER_TOKEN_PREFIX: String = "Bearer"
+    }
+
+    @Bean
+    fun openAPI(): OpenAPI {
+        val securityJwtName = "JWT"
+        val securityRequirement = SecurityRequirement().addList(securityJwtName)
+        val components =
+            Components()
+                .addSecuritySchemes(
+                    securityJwtName,
+                    SecurityScheme()
+                        .name(securityJwtName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(BEARER_TOKEN_PREFIX)
+                        .bearerFormat(securityJwtName),
+                )
+        return OpenAPI()
+            .addSecurityItem(securityRequirement)
+            .components(components)
+    }
+}
