58ccce89b489dc825e176d27ecf9ba68e4bcc81f
Ewan Jee <830jiforcareer@gmail.com>
2024-06-13 23:42:20 +0900

    querydsl 작동 잘 되는지 확인

--- src/main/kotlin/com/nasa/todaynasa/adapter/out/persistence/apod/ApodPersistenceAdapter.kt
+++ src/main/kotlin/com/nasa/todaynasa/adapter/out/persistence/apod/ApodPersistenceAdapter.kt
@@ -1,7 +1,15 @@
 package com.nasa.todaynasa.adapter.out.persistence.apod
 
 import com.nasa.todaynasa.application.port.out.apod.ApodCommandPort
+import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Component
 
 @Component
-class ApodPersistenceAdapter : ApodCommandPort
+class ApodPersistenceAdapter(
+    private val apodRepository: ApodRepository,
+    private val queryFactory: JPAQueryFactory,
+) : ApodCommandPort {
+    fun h()  {
+        queryFactory.from(QApodEntity.apodEntity).fetch()
+    }
+}
