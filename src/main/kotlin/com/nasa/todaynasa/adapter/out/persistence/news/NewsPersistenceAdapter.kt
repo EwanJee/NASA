f7e29738ce932bc8adfd539394ff6f5385b9914d
Ewan Jee <830jiforcareer@gmail.com>
2024-07-17 23:34:36 +0900

    add : implement 도입

--- src/main/kotlin/com/nasa/todaynasa/adapter/out/persistence/news/NewsPersistenceAdapter.kt
+++ src/main/kotlin/com/nasa/todaynasa/adapter/out/persistence/news/NewsPersistenceAdapter.kt
@@ -2,6 +2,14 @@ package com.nasa.todaynasa.adapter.out.persistence.news
 
 import com.nasa.todaynasa.application.port.out.news.NewsCommandPort
 import org.springframework.stereotype.Component
+import java.time.LocalDate
 
 @Component
-class NewsPersistenceAdapter : NewsCommandPort
+class NewsPersistenceAdapter : NewsCommandPort {
+    override fun getNewsByTopicAndDate(
+        topic: String,
+        date: LocalDate,
+    ) {
+        TODO("Not yet implemented")
+    }
+}
