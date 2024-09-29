bbca4d6a58da181edb9da547ef59cd66ef416e91
Ewan Jee <830jiforcareer@gmail.com>
2024-09-29 17:13:14 -0400

    feat: Adapter 어노테이션 추가

---
+++ src/main/kotlin/com/nasa/todaynasa/infrastructure/annotations/Adapter.kt
@@ -0,0 +1,12 @@
+package com.nasa.todaynasa.infrastructure.annotations
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Component
+annotation class Adapter(
+    @get:AliasFor(annotation = Component::class)
+    val value: String = "",
+)
